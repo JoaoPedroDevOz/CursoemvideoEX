990488554004a024705bf2383b9072d87c9ead14
João Pedro <101525753+JoaoPedroDevOz@users.noreply.github.com>
2022-03-15 23:01:52 -0300

    Rename exercício 6.py to exercício 06.py

--- Exercices/exercício 6.py
+++
@@ -1,7 +0,0 @@
-n1 = int(input('Type a value:'))
-
-totaldob = n1*2
-totaltri = n1*3
-totalsqr = n1**(1/2)
-
-print("The double's number: {},\n The triple's number: {},\n The square's root: {:.2f}".format(totaldob, totaltri, totalsqr))
